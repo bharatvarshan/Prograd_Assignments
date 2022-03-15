b98b4c714c589d5485c3dbc6b224bbd57a1c0a46
BHARAT VARSHAN S M <bharatvarshan.cse@gmail.com>
2022-03-16 01:05:28 +0530

    made modifications

--- day4/createArray.java
+++ day4/createArray.java
@@ -2,6 +2,7 @@ package dailyCoding.day4;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class createArray {
     public static void main(String[] args) {
@@ -9,12 +10,23 @@ public class createArray {
         ArrayList<String> cars = new ArrayList<>();
         System.out.println("Enter Names of cars with space");
 
+        String inputString = scanner.nextLine();
 
+        StringTokenizer stringTokenizer = new StringTokenizer(inputString, " ");
 
-        System.out.println(cars);
+        while(stringTokenizer.hasMoreTokens()){
+            cars.add(stringTokenizer.nextToken());
+        }
 
+        //Not working
+//        while (scanner.hasNext(" ")){
+//            cars.add(scanner.next());
+//        }
+
+//        System.out.println(cars);
+        System.out.println("Great Choices!!");
         for (String car : cars) {
-            System.out.println(car);
+            System.out.print(car + " ");
         }
     }
 }
