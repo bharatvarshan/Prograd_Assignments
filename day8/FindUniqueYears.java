d0bd09ef7c3df7b6dd91e03d5f70ad7218c069ec
BHARAT VARSHAN S M <bharatvarshan.cse@gmail.com>
2022-03-16 17:41:59 +0530

    Added methods

--- day8/FindUniqueYears.java
+++ day8/FindUniqueYears.java
@@ -8,25 +8,29 @@ import java.util.regex.Pattern;
 
 public class FindUniqueYears{
 
+    public static ArrayList<String> findDates(String string){
+        Pattern pattern = Pattern.compile("(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-\\d{4}");
+        Matcher matcher = pattern.matcher(string);
+        ArrayList<String> dates = new ArrayList<String>();
+        while (matcher.find()){
+            dates.add(matcher.group());
+        }
+        return dates;
+    }
+
+    public static int findNoOfUniqueYears(ArrayList<String> datesList ){
+        HashSet<String> years = new HashSet<>();
+        for(String date : datesList){
+            years.add(date.substring(6,10));
+        }
+        return years.size();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the String:");
         String inputString = scanner.nextLine();
-        Pattern pattern = Pattern.compile("(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-\\d{4}");
-        Matcher matcher = pattern.matcher(inputString);
-
-        ArrayList<String> dates = new ArrayList<String>();
-        HashSet<String> years = new HashSet<>();
-
-        while (matcher.find()){
-            dates.add(matcher.group());
-        }
-
-        for(String date : dates){
-            years.add(date.substring(6,10));
-        }
-
-        System.out.println(years.size());
-
+        System.out.print("No of unique Years are: ");
+        System.out.println(findNoOfUniqueYears(findDates(inputString)));
     }
 }
