caf2e7c9f453d2017ffb246322c132d3174f4a81
bharatvarshan <bharatvarshan.cse@gmail.com>
2022-03-22 23:43:35 +0530

    Day 12 code CheckValidIP

---
+++ day12/CheckIP.java
@@ -0,0 +1,46 @@
+package dailyCoding.day12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CheckIP {
+
+    public static boolean checkInt(String string){
+        for(int i = 0; i < string.length();i++){
+            if(string.charAt(i) >=0 && string.charAt(i) <= 9){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean validateIP(String string){
+        String[] array = string.split("\\.");
+        if(array.length != 4){
+            return false;
+        }
+
+        for(int i =0 ; i < array.length ; i++){
+            //array[i].matches("[0-9]+")  checkInt(array[i])
+            if(array[i].matches("[0-9]+")){
+                int bytePart = Integer.parseInt(array[i]);
+                if (bytePart < 0 || bytePart > 255){
+                    return false;
+                }
+                if (i == 3 && bytePart == 0 || bytePart == 255 ){
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputIP = scanner.nextLine();
+        System.out.println(validateIP(inputIP) ? "Valid IP" : "Invalid IP");
+    }
+
+}
