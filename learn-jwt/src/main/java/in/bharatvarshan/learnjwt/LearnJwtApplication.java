45d39f995e3bc83e4c2a837c0e51970ef1469411
BHARAT VARSHAN S M <bv@BHARATs-MacBook-Air.local>
2022-04-01 20:41:02 +0530

    [LEARN] Spring Security Application with JWT

---
+++ learn-jwt/src/main/java/in/bharatvarshan/learnjwt/LearnJwtApplication.java
@@ -0,0 +1,13 @@
+package in.bharatvarshan.learnjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnJwtApplication.class, args);
+    }
+
+}
---
+++ learn-jwt/src/test/java/in/bharatvarshan/learnjwt/LearnJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bharatvarshan.learnjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
